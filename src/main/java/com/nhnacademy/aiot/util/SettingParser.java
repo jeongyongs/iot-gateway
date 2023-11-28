5afd84ee0e9ec2d787e54685cc49cbb653d90ca9
NamJiMin <njmin8@naver.com>
2023-11-28 17:25:12 +0900

    feat: args에서 옵션 분석하는 기능 추가

--- src/main/java/com/nhnacademy/aiot/util/SettingParser.java
+++ src/main/java/com/nhnacademy/aiot/util/SettingParser.java
@@ -6,17 +6,28 @@ import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import com.nhnacademy.aiot.setting.Setting;
 
+/**
+ * commnad line에서 application name과 sensor type을 분석하는 클래스입니다.
+ */
 public class SettingParser {
     private static final String DEFAULT_APPLICATION_NAME = "#/";
-    private static final String DEFAULT_SENSOR_TYPE = "temperature";
+    private static final JSONArray DEFAULT_SENSOR_TYPE = new JSONArray().put("temperature");
 
     private SettingParser() {
     }
 
+    /**
+     * command line을 분석합니다.
+     * 
+     * @param args main에서 입력한 String 배열
+     * @return application name은 <code>"an"</code>, sensor type은 JSONArray로
+     *         <code>"s"</code>라는 key에 담은 <code>JSONObject</code>
+     */
     public static Setting parse(String[] args) {
         Setting setting = new Setting();
         Options options = new Options();
@@ -34,10 +45,10 @@ public class SettingParser {
                 setting.put("an", commandLine.getOptionValue("an"));
             }
             if (commandLine.hasOption("s")) {
-                setting.put("s", commandLine.getOptionValue("s"));
+                setting.put("s", new JSONArray(commandLine.getOptionValue("s").split(",")));
             }
             if (setting.isNull("an") || setting.isNull("s")) {
-                fillNull(setting, commandLine.hasOption("c"));
+                fillNull(setting, commandLine);
             }
         } catch (ParseException e) {
             HelpFormatter formatter = new HelpFormatter();
@@ -46,29 +57,30 @@ public class SettingParser {
         return setting;
     }
 
-    private static void fillNull(Setting setting, boolean hasOptionC) {
-        if (!hasOptionC) {
+    /**
+     * args에서 입력하지 않은 옵션을 기본값이나 설정 파일의 옵션으로 설정합니다.
+     * 
+     * @param setting     수정할 JSONObject
+     * @param commandLine 읽어야 할 commandLine
+     */
+    private static void fillNull(Setting setting, CommandLine commandLine) {
+        // 설정 파일이 있을 때
+        if (commandLine.hasOption("c")) {
+            JSONObject settingJSON = JSONFileReader.read(commandLine.getOptionValue("c"));
+            if (setting.isNull("an")) {
+                setting.put("an", settingJSON.getString("an"));
+            }
+            if (setting.isNull("s")) {
+                setting.put("s", settingJSON.getJSONArray("s"));
+            }
+        // 설정 파일이 없을 때 (기본값으로 설정)
+        } else {
             if (setting.isNull("an")) {
                 setting.put("an", DEFAULT_APPLICATION_NAME);
             }
             if (setting.isNull("s")) {
-                setting.put("an", DEFAULT_APPLICATION_NAME);
+                setting.put("s", DEFAULT_SENSOR_TYPE);
             }
         }
-        else {
-            
-            if (setting.isNull("an")) {
-                setting.put("an", DEFAULT_APPLICATION_NAME);
-            }
-            if (setting.isNull("s")) {
-                setting.put("an", DEFAULT_APPLICATION_NAME);
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        // SettingParser.parse(args);
-        JSONObject o = new JSONObject();
-        System.out.println(o.isNull("an"));
     }
 }
