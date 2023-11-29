4e6349c064b5f08592955b2e2493d8c1025ebdac
{joohyeon98} <{ljh221803@gmail.com}>
2023-11-29 09:58:13 +0900

    feat: MQTTOutNode

---
+++ src/main/java/com/nhnacademy/aiot/node/impl/MQTTOutNode.java
@@ -0,0 +1,68 @@
+package com.nhnacademy.aiot.node.impl;
+
+import java.util.ArrayList;
+import org.eclipse.paho.client.mqttv3.IMqttClient;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import com.nhnacademy.aiot.node.impl.OutNole;
+import com.nhnacademy.aiot.pipe.Pipe;
+
+@Slf4j
+public class MQTTOutNode implements Outputable, Runnable {
+    private static MQTTOutNode mqttOutNode;
+    private static Info info;
+    private static ArrayList<Pipe> pipeList = new ArrayList<>();
+
+    private MQTTOutNode() {
+        super();
+    }
+
+    public static MQTTOutNode getMqttOutNode() {
+        if (mqttOutNode == null) {
+            mqttOutNode = new MQTTOutNode();
+        }
+        return mqttOutNode;
+    }
+
+    // info (increase _recieve, fail)
+    // try-catch 발생 -> fail increase
+    @Override
+    public void connectInPipe(Pipe outPipe) {
+        for (Pipe pipeList : outPipe) {
+            if (pipeList.put != null) {
+                return Info.increase;
+            }
+
+        }
+    }
+
+    @Override
+    public void run() {
+        // packet 받아와서
+        try (IMqttClient client = new MqttClient("tcp://localhost:1883", "myId")) {
+            log.info("connect");
+            MqttConnectOptions options = new MqttConnectOptions();
+            options.setWill("test/will", "Disconnected".getBytes(), 1, true);
+            client.connect(options);
+
+            client.publish(getName(), null); // packet 보내기
+
+            while (!Thread.currentThread().isInterrupted()) {
+                Thread.sleep(100);
+            }
+
+            client.disconnect();
+            log.info("disconenct");
+        } catch (MqttException e) {
+            e.printStackTrace();
+            System.out.println("connect fail");
+        }
+    }
+
+    // publish
+    public void publish() {
+        Thread.start();
+    }
+}
+
---
+++ src/test/java/com/nhnacademy/aiot/node/MQTTOutNodeTest.java
@@ -0,0 +1,10 @@
+package com.nhnacademy.aiot.node;
+
+import org.junit.jupiter.api.Test;
+
+public class MQTTOutNodeTest {
+    @Test
+    void testGetMqttOutNode() {
+
+    }
+}
