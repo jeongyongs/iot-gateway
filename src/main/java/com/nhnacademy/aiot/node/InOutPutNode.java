611893448ffe720004dad4bac86815de203cc1f0
Maru375 <www980304@gmail.com>
2023-11-30 13:38:22 +0900

    refactor: inoutputnode
    입력 및 출력을 처리하는 추상 클래스
    페킷을 꺼내고, 넣고,  노드를 연결하는 동작을 지원

--- src/main/java/com/nhnacademy/aiot/node/InOutPutNode.java
+++ src/main/java/com/nhnacademy/aiot/node/InOutPutNode.java
@@ -6,64 +6,53 @@ import com.nhnacademy.aiot.port.Port;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.Optional;
 
+/**
+ * InOutPutNode는 입력 및 출력을 처리하는 노드의 추상 클래스입니다.
+ */
 public abstract class InOutPutNode extends Node {
     private Port inPorts;
     private Map<Integer, Port> outPorts;
-
+    
     protected InOutPutNode() {
-        this.inPorts = new Port() {
-
-            @Override
-            public void add(Pipe pipe) {
-                // TODO Auto-generated method stubPort inPort, Map<Integer, Port> outPorts
-                throw new UnsupportedOperationException("Unimplemented method 'add'");
-            }
-
-            @Override
-            public boolean put(Packet packet) {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'put'");
-            }
-
-            @Override
-            public Optional<Packet> take() {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'take'");
-            }
-
-        };
-        this.outPorts = new HashMap<>();
+        inPorts = new Port();
+        outPorts = new HashMap<>();
     }
 
     /**
-     * Port
-     * 
-     * @return
-     * @throws NoSuchElementException
+     * 입력을 받아오는 메서드입니다.
+     * @return 입력으로 받아온 패킷
      */
-    protected Packet receive() throws NoSuchElementException {
-        Optional<Packet> packet = inPorts.take();
-
-        if (packet.isPresent()) {
-            return packet.get();
-        }
-        throw new NoSuchElementException();
+    protected Packet receive() {
+        return inPorts.take();
     }
 
+    /**
+     * 지정된 포트로 패킷을 전송하는 메서드입니다.
+     *
+     * @param portNum 전송할 포트 번호
+     * @param packet  전송할 패킷
+     */
     protected void send(int portNum, Packet packet) {
-        Port outPort = outPorts.get(portNum);                
+        Port outPort = outPorts.get(portNum);
         if (outPort != null) {
             outPort.put(packet);
         }
     }
 
+    /**
+     * 입력 파이프를 연결하는 메서드입니다.
+     * @param pipe 연결할 입력 파이프
+     */
     public void connectInPipe(Pipe pipe) {
         inPorts.add(pipe);
     }
 
+    /**
+     * 지정된 포트에 출력 파이프를 연결하는 메서드입니다.
+     * @param portNum 연결할 출력 포트 번호
+     * @param pipe    연결할 출력 파이프
+     */
     public void connectOutPipe(int portNum, Pipe pipe) {
         Port outPort = outPorts.get(portNum);
         if (outPort != null) {
