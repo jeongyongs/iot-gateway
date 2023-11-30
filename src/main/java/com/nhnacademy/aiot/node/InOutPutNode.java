ca38c60c9df4f4677f6f35c0b01fc28b2c8f40d9
Maru375 <www980304@gmail.com>
2023-11-30 15:56:38 +0900

    fix: inoutputnode
    - inPorts 필드 final 추가
    - 슈퍼클래스의 생성자를 실행하기위한 super 추가
    - send에서 containsKey()를 사용하도록 변경
    - connectOutPipe 불필요한 과정 삭제
    
    Resolves: #10

--- src/main/java/com/nhnacademy/aiot/node/InOutPutNode.java
+++ src/main/java/com/nhnacademy/aiot/node/InOutPutNode.java
@@ -11,16 +11,18 @@ import java.util.Map;
  * InOutPutNode는 입력 및 출력을 처리하는 노드의 추상 클래스입니다.
  */
 public abstract class InOutPutNode extends Node {
-    private Port inPorts;
-    private Map<Integer, Port> outPorts;
-    
+    private final Port inPorts;
+    private final Map<Integer, Port> outPorts;
+
     protected InOutPutNode() {
+        super();
         inPorts = new Port();
         outPorts = new HashMap<>();
     }
 
     /**
      * 입력을 받아오는 메서드입니다.
+     * 
      * @return 입력으로 받아온 패킷
      */
     protected Packet receive() {
@@ -34,14 +36,15 @@ public abstract class InOutPutNode extends Node {
      * @param packet  전송할 패킷
      */
     protected void send(int portNum, Packet packet) {
-        Port outPort = outPorts.get(portNum);
-        if (outPort != null) {
+        if (outPorts.containsKey(portNum)) {
+            Port outPort = outPorts.get(portNum);
             outPort.put(packet);
         }
     }
 
     /**
      * 입력 파이프를 연결하는 메서드입니다.
+     * 
      * @param pipe 연결할 입력 파이프
      */
     public void connectInPipe(Pipe pipe) {
@@ -50,13 +53,12 @@ public abstract class InOutPutNode extends Node {
 
     /**
      * 지정된 포트에 출력 파이프를 연결하는 메서드입니다.
+     * 
      * @param portNum 연결할 출력 포트 번호
      * @param pipe    연결할 출력 파이프
      */
     public void connectOutPipe(int portNum, Pipe pipe) {
         Port outPort = outPorts.get(portNum);
-        if (outPort != null) {
-            outPort.add(pipe);
-        }
+        outPort.add(pipe);
     }
 }
