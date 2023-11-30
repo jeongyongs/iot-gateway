72260c7404ef5a7221fba512f25346a374a4e50a
pangpangE123 <eric0420@naver.com>
2023-11-30 14:29:43 +0900

    수정

--- src/main/java/com/nhnacademy/aiot/node/InputNode.java
+++ src/main/java/com/nhnacademy/aiot/node/InputNode.java
@@ -1,50 +1,43 @@
 package com.nhnacademy.aiot.node;
 
-import java.util.List;
 import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.Optional;
 import com.nhnacademy.aiot.port.Packet;
 import com.nhnacademy.aiot.port.Pipe;
 import com.nhnacademy.aiot.port.Port;
 
 public abstract class InputNode extends Node {
 
-    private Map<Integer, Port> outPorts;
-    private Port inPort;
+    protected Map<Integer, Port> outPorts;
+    protected Port inPort;
 
-    protected void connectOutPort(int portNumber, Pipe pipe) {
-
-        // 이 노드의 portNumber번의 outPort에 잇는 pipe와 연결
-        outPorts.get(portNumber).add(pipe);
-
+    // 디폴트 포트넘버 0
+    public void connectOutPort(Pipe pipe) {
+        outPorts.get(0).add(pipe);
     }
 
-    protected void connectInputPort(Pipe pipe) {
+    // 포트 넘버 지정
+    public void connectOutPort(int portNumber, Pipe pipe) {
+        // 이 노드의 portNumber번의 outPort에 잇는 pipe와 연결
+        outPorts.get(portNumber).add(pipe);
+    }
 
+    public void connectInputPort(Pipe pipe) {
         // 이 노드의 inputPort예 잇는 pipe와 연결
         inPort.add(pipe);
     }
 
+    // 디폴트 포트넘버 0
+    protected void send(Packet packet) {
+        outPorts.get(0).put(packet);
+    }
+
+    // 포트 넘버 지정
     protected void send(int portNumber, Packet packet) {
         outPorts.get(portNumber).put(packet);
     }
 
     protected Packet receive() {
-        return inPort.get();
+        return inPort.take();
     }
 
-
-    // port class
-    // public boolean put(Packet packet) {
-
-    // for (Pipe Pipe : Pipes) {
-    // sendPipe.put(packet);
-    // }
-
-    // return true;
-
-    // }
 }
-
-
