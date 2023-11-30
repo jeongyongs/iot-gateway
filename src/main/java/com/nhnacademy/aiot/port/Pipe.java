0e5351723eabdb79f64fa32c6a81c858b5941ac0
woooyas <njmin8@naver.com>
2023-11-30 16:28:05 +0900

    fix: 예외 처리 수정

--- src/main/java/com/nhnacademy/aiot/port/Pipe.java
+++ src/main/java/com/nhnacademy/aiot/port/Pipe.java
@@ -1,6 +1,5 @@
 package com.nhnacademy.aiot.port;
 
-import java.util.Optional;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -9,38 +8,40 @@ import java.util.concurrent.LinkedBlockingQueue;
  */
 public class Pipe {
 
-    BlockingQueue<Packet> queue = new LinkedBlockingQueue<>();
+    private BlockingQueue<Packet> queue = new LinkedBlockingQueue<>();
 
     /**
      * pipe에 packet을 넣습니다.
      * 
      * @param packet pipe에 넣을 packet
      * @return packet을 정상적으로 넣었으면 true, 아니면 flase
+     * @throws InterruptedException
      */
     public boolean put(Packet packet) {
+        boolean flag = false;
         try {
             queue.put(packet);
-            return true;
+            flag = true;
         } catch (InterruptedException ignore) {
             Thread.currentThread().interrupt();
-            return false;
         }
+        return flag;
     }
 
     /**
      * pipe에서 packet을 꺼냅니다.
      * 
-     * @return pipe에서 정상적으로 메세지를 꺼냈다면 value가 packet인 optional, 아니면 value가 null인 optional
+     * @return pipe에서 정상적으로 메세지를 꺼냈다면 value가 packet인 optional, 아니면 value가 null인
+     *         optional
      * @throws InterruptedException
      */
-    public Optional<Packet> take() {
-        Packet packet = null;
+    public Packet take() {
         try {
-            packet = queue.take();
-        } catch (InterruptedException ignore) {
+            return queue.take();
+        } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
+            return null;
         }
-        return Optional.ofNullable(packet);
     }
 
     /**
