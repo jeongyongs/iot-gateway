63f1c17b852dde253a6205c03462e496f3474280
Maru375 <www980304@gmail.com>
2023-11-30 15:14:41 +0900

    feat: FunctionNode
    전달 받은 요구사항에 맞게 새롭게 패킷을 만들어 다음노드로 전달하는 클래스 입니다.

---
+++ src/main/java/com/nhnacademy/aiot/node/FunctionNode.java
@@ -0,0 +1,31 @@
+package com.nhnacademy.aiot.node;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+
+import com.nhnacademy.aiot.port.Packet;
+
+/**
+ * 받은 페킷을 전달 받은 요구사항에 맞게 재구성하여
+ * 다음 노드로 페킷을 전달 합니다.
+ */
+public class FunctionNode extends InOutPutNode {
+    private final Function<Packet, List<Packet>> function;
+
+    /**
+     * @param function 페킷 구성 요구 사항
+     */
+    public FunctionNode(Function<Packet, List<Packet>> function) {
+        super();
+        this.function = function;
+    }
+
+    @Override
+    protected void process() {
+        Packet packet = receive();
+        List<Packet> packetList = function.apply(packet);
+        IntStream.range(0, packetList.size())
+                .forEach(i -> send(i, packetList.get(i)));
+    }
+}
