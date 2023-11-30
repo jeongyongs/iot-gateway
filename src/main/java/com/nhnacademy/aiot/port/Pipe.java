f0cee42af4c4d4f237d8d6768f460bc286975071
woooyas <njmin8@naver.com>
2023-11-30 18:10:48 +0900

    refactor: 변수명 수정 및 로그 추가

--- src/main/java/com/nhnacademy/aiot/port/Pipe.java
+++ src/main/java/com/nhnacademy/aiot/port/Pipe.java
@@ -3,43 +3,46 @@ package com.nhnacademy.aiot.port;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
+import lombok.extern.slf4j.Slf4j;
+
 /*
  * packet이 node간에 이동할 수 있도록 하는 클래스입니다.
  */
+
+ @Slf4j
 public class Pipe {
 
-    private BlockingQueue<Packet> queue = new LinkedBlockingQueue<>();
+    private final BlockingQueue<Packet> queue = new LinkedBlockingQueue<>();
 
     /**
      * pipe에 packet을 넣습니다.
      * 
      * @param packet pipe에 넣을 packet
      * @return packet을 정상적으로 넣었으면 true, 아니면 flase
-     * @throws InterruptedException
      */
     public boolean put(Packet packet) {
-        boolean flag = false;
+        boolean isPacketSent = false;
         try {
             queue.put(packet);
-            flag = true;
+            isPacketSent = true;
         } catch (InterruptedException ignore) {
             Thread.currentThread().interrupt();
+            log.debug("InterruptedException 발생");
         }
-        return flag;
+        return isPacketSent;
     }
-
+    
     /**
      * pipe에서 packet을 꺼냅니다.
      * 
-     * @return pipe에서 정상적으로 메세지를 꺼냈다면 value가 packet인 optional, 아니면 value가 null인
-     *         optional
-     * @throws InterruptedException
+     * @return pipe에서 정상적으로 메세지를 꺼냈다면 value가 packet인 optional, InterruptedException가 발생하면 null
      */
     public Packet take() {
         try {
             return queue.take();
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
+            log.debug("InterruptedException 발생");
             return null;
         }
     }
