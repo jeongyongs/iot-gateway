b8ce9b39e459109f333dcd1ca306dc3a6129c322
woooyas <njmin8@naver.com>
2023-12-11 18:25:19 +0900

    feat: functionFactory 구현

--- src/main/java/com/nhnacademy/aiot/util/FunctionFactory.java
+++ src/main/java/com/nhnacademy/aiot/util/FunctionFactory.java
@@ -13,6 +13,12 @@ public class FunctionFactory {
     private FunctionFactory() {
     }
 
+    /**
+     * string인 script를 실행하는 Function 인스턴스를 생성합니다.
+     * 
+     * @param script functionNode가 실행할 코드
+     * @return script를 실행할 수 있는 Function<Packet, List<Packet>> 인스턴스
+     */
     @SuppressWarnings("unchecked")
     public static Function<Packet, List<Packet>> getInstance(String script) {
         GroovyShell shell = new GroovyShell();
